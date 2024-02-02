03cda4c16f9919aba3ca65eb30c9a0fcdafce90e
WillDizzle <157166862+WillDizzle@users.noreply.github.com>
2024-02-02 14:30:22 -0500

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Willie Rao>
+ *        Date: <1/26/2024>
+ * Description: Lab2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
